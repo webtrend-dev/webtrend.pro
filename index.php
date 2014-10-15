9d8bbb0744f37792f1d11345e36cfc433d9e00bd
andreybulankin <andrey.bulankin@gmail.com>
2014-10-15 16:00:03 +0400

    Первичная загрузка файлов в репозиторий

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <title>webtrend.pro</title>
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/1.0.0.css" rel="stylesheet">
+</head>
+<body>
+<header class="header">
+  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="/">web trend</a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#o-kompanii">о компании</a></li>
+          <li><a href="#uslugi">услуги</a></li>
+          <li class="active"><a href="#portfolio">портфолио</a></li>
+          <li><a href="#kontakty">услуги</a></li>
+          <li><a href="#blog">блог</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+</header>
+<div class="top-slider">
+  <div class="tab-content">
+    <div class="tab-pane fade in active" id="top-slider-1">
+      <div class="container">
+        <div class="jumbotron">
+          <p class="h1">Hello, world!</p>
+          <p>с высокой конверсией всего за</p>
+          <p class="price">99 900 рублей</p>
+          <p><a class="btn btn-primary btn-lg" role="button">Подробнее</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="tab-pane fade in" id="top-slider-2">
+      <div class="container">
+        <div class="jumbotron">
+          <p class="h1">Hello, world!</p>
+          <p>с высокой конверсией всего за</p>
+          <p>99 900 рублей</p>
+          <p><a class="btn btn-primary btn-lg" role="button">Подробнее</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="tab-pane fade in" id="top-slider-3">
+      <div class="container">
+        <div class="jumbotron">
+          <p class="h1">Hello, world!</p>
+          <p>с высокой конверсией всего за</p>
+          <p>99 900 рублей</p>
+          <p><a class="btn btn-primary btn-lg" role="button">Подробнее</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="tab-pane fade in" id="top-slider-4">
+      <div class="container">
+        <div class="jumbotron">
+          <p class="h1">Hello, world!</p>
+          <p>с высокой конверсией всего за</p>
+          <p>99 900 рублей</p>
+          <p><a class="btn btn-primary btn-lg" role="button">Подробнее</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="tab-list">
+    <div class="container">
+      <ul class="list-inline" role="tablist">
+        <li class="col-xs-3 active"><a href="#top-slider-1" role="tab" data-toggle="tab">Скидка 25%</a></li>
+        <li class="col-xs-3"><a href="#top-slider-2" role="tab" data-toggle="tab">Интернет-магазин</a></li>
+        <li class="col-xs-3"><a href="#top-slider-3" role="tab" data-toggle="tab">Рекордсмены</a></li>
+        <li class="col-xs-3"><a href="#top-slider-4" role="tab" data-toggle="tab">Лидеры</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="company-stats">
+  <div class="container">
+    <ul class="list-inline">
+      <li class="col-xs-4"><span>78</span> довольных клиентов</li>
+      <li class="col-xs-4"><span>116</span> действующих проектов</li>
+      <li class="col-xs-4"><span>78</span> работ в портфолио</li>
+    </ul>
+  </div>
+</div>
+<div class="services" id="uslugi">
+  <header class="panel panel-red">
+    <div class="container">
+      <div class="jumbotron">
+        <div class="h1">Услуги</div>
+        <div class="panel-preview">Мы растущая компания, которая фокусируется<br> на разработке, проектировании, поддержке сайтов</div>
+        <div class="panel-legend">
+          <!-- разработка, сопровождение, проектирвоание, аналитика -->
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="section-description">
+    <div class="container">
+      <p>Awesome text</p>
+    </div>
+  </div>
+  <div class="services-site-creating" id="services-site-creating">
+    <div class="h2">Разработка</div>
+    <div class="services-workflow"><!--img src=""--></div>
+    <div class="panel panel-black">
+      <div class="h3">Технологии</div>
+      <ul class="list-inline list-technologies">
+        <li><span class="icon icon-html5"></span></li>
+        <li><span class="icon icon-css3"></span></li>
+        <li><span class="icon icon-axure"></span></li>
+        <li><span class="icon icon-javascript"></span></li>
+        <li><span class="icon icon-php"></span></li>
+        <li><span class="icon icon-sql"></span></li>
+        <li><span class="icon icon-schema"></span></li>
+      </ul>
+    </div>
+    <div class="panel panel-red">
+      <div class="h3">Партнеры</div>
+      <ul class="list-inline list-partners">
+        <li><span class="icon icon-umi"></span></li>
+        <li class="icon icon-bitrix"></li>
+      </ul>
+    </div>
+    <div class="container">
+      <div class="col-xs-6">Сайты услуг</div>
+      <div class="col-xs-6">Интернет-магазин</div>
+    </div>
+  </div>
+</div>
+<footer>
+
+</footer>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
