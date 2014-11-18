2e9ad0f1fc6def3f0bfd6fc8846eff3b02cf0732
IvanKrasnikov <test@tests-iMac.local>
2014-11-18 19:09:56 +0400

    страница Услуги

---
+++ uslugi.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <title>webtrend.pro - Услуги</title>
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/1.0.0.css" rel="stylesheet">
+</head>
+<body data-spy="scroll" data-target=".navbar-example">
+  <header class="header low-header">
+    <div class="navbar navbar-inverse " role="navigation"><!--navbar-fixed-top-->
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="/">web trend</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#o-kompanii">о компании</a></li>
+            <li class="active"><a href="#uslugi">услуги</a></li>
+            <li><a href="#portfolio">портфолио</a></li>
+            <li><a href="#kontakty">блог</a></li>
+            <li><a href="#blog">контакты</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+  </header>
+  
+  <div class="services" id="uslugi">
+    <header class="panel-red-big">
+      <div class="container">
+        <div class="jumbotron">
+          <div class="h1">Услуги</div>
+          <div class="panel-preview">Мы растущая компания, которая фокусируется<br> на разработке, проектировании, поддержке сайтов</div>
+          <div class="panel-legend">
+            <div class="container">
+              <div class="uslugi-legend-map">
+	            <div class="uslugi-point active"></div>
+	            <div class="uslugi-point p2"></div>
+	            <div class="uslugi-point p3"></div>
+	            <div class="uslugi-point p4"></div>
+              </div>
+              <ul class="list-inline uslugi-main-buttons">
+                <li class="col-xs-3"><a href="#to-razrabotka" class="button-razrabotka active">Разработка</a></li>
+                <li class="col-xs-3"><a href="#services-site-accompaniment" class="button-soprovozhdenie">Сопровождение</a></li>
+                <li class="col-xs-3"><a href="#to-proektirovanie" class="button-proektirovanie">Проектирование</a></li>
+                <li class="col-xs-3"><a href="#to-analitika" class="button-analitika">Аналитика</a></li>
+              </ul>
+            </div>
+          <!-- разработка, сопровождение, проектирвоание, аналитика -->
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="uslugi-section-description">
+      <div class="container">
+        <p class="uslugi-text">Мы проектируем и создаем сайты как единую систему для решения поставленных заказчиком<br> задач. Глубоко погружаемся в проект, каждую страницу рассматриваем как интерфейс,<br> требующий проработки контента. Сразу продумываем рекламные кампании.</p>
+        <p>
+	      <a href="#" class="btn btn-black-red btn-lg btn-244">Подробнее</a>
+        </p>
+      </div>
+    </div>
+    <div class="panel-services-menu">
+	  <div class="panel-services-menu-red">
+        <div class="container">
+	      <div class="uslugi-buttons-panel">
+	        <div class="container navbar-example">
+		      <ul class="list-inline uslugi-buttons-small nav nav-tabs" role="tablist">
+		        <li class="col-xs-3"><a href="#services-site-creating" name="services-site-creating" class="button-razrabotka-small active">Разработка</a></li>
+		        <li class="col-xs-3"><a href="#services-site-accompaniment" name="services-site-accompaniment" class="button-soprovozhdenie-small">Сопровождение</a></li>
+		        <li class="col-xs-3"><a href="#services-engineering" name="services-engineering" class="button-proektirovanie-small">Проектирование</a></li>
+		        <li class="col-xs-3"><a href="#services-analitics" name="services-analitics" class="button-analitika-small">Аналитика</a></li>
+		      </ul>
+	        </div>
+	      </div>
+        </div>
+      </div>
+    </div>
+    <div class="services-site-creating" id="services-site-creating">
+      <div class="h2 bottom-red-line">Разработка</div>
+      <div class="services-workflow site-creating-img">
+        <img src="./images/services-algorithm.png" alt=""><!-- 1100px -->
+      </div>
+      <div class="panel-black panel-tecnologies">
+        <div class="h3">Технологии</div>
+        <ul class="list-inline list-technologies">
+          <li><span class="icon icon-html5"></span></li>
+          <li><span class="icon icon-css3"></span></li>
+          <li><span class="icon icon-axure"></span></li>
+          <li><span class="icon icon-javascript"></span></li>
+          <li><span class="icon icon-php"></span></li>
+          <li><span class="icon icon-sql"></span></li>
+          <li><span class="icon icon-schema">schema.org</span></li>
+        </ul>
+      </div>
+      <div class="panel-red panel-partners">
+        <div class="h3">Партнеры</div>
+        <ul class="list-inline list-partners">
+          <li><span class="icon icon-umi">золотой партнер</span></li>
+          <li><span class="icon icon-bitrix"></span></li>
+        </ul>
+      </div>
+      <div class="services-description">
+        <div class="container">
+          <div class="col-xs-6 services-description-column">
+            <div class="h4 saiti-uslug">Сайты услуг</div>
+            <p class="services-desc-text">Заказав сайт у нас, вы можете быть уверены — этот ресурс станет не только визитной карточкой вашей компании, но и  — эффективным инструментом, неизменно приносящим новых клиентов и процветание бизнесу. </p>
+            <div class="services-link-portfolio"><a href="#" class="red-link">Посмотреть в портфолио</a></div>
+            <div class="services-link-dogovor"><a href="#" class="black-link">Скачать договор</a></div>
+            <a href="#" class="btn btn-black-red know-prices">Узнать цены</a>
+          </div>
+          <div class="col-xs-6 services-description-column">
+            <div class="h4 internet-magazin">Интернет-магазин</div>
+            <p class="services-desc-text">Создавая интернет-магазин в Web-trend, ресурс, который осуществляет продажи на протяжении 24 часов в сутки, 7 дней в неделю, вы вкладываете будущее. Интернет-магазин работает на вас, даже когда вы отдыхаете.</p>
+            <div class="services-link-portfolio"><a href="#" class="red-link">Посмотреть в портфолио</a></div>
+            <div class="services-link-dogovor"><a href="#" class="black-link">Скачать договор</a></div>
+            <a href="#" class="btn btn-black-red know-prices">Узнать цены</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="services-site-accompaniment" id="services-site-accompaniment">
+      <div class="h2 bottom-red-line">Сопровождение</div>
+      <div class="services-workflow">
+        <img src="./images/accompaniment-algorithm.png" alt=""><!-- 880px -->
+      </div>
+      <div class="accompaniment-stats"> 
+        <div class="container">
+          <ul class="list-inline clearfix">
+            <li class="col-xs-4">
+              <div class="comma-big-outer"><span class="comma-big-inner">157</span></div>
+              <span class="accompaniment-stats-description">клиентов<br> на сопровожение</span>
+            </li>
+            <li class="col-xs-4">
+              <div class="comma-big-outer"><span class="comma-big-inner">11.257</span></div>
+              <span class="accompaniment-stats-description">выполнено<br> задач</span>
+            </li>
+            <li class="col-xs-4">
+              <div class="comma-big-outer"><span class="comma-big-inner">3.492</span></div>
+              <span class="accompaniment-stats-description">отработано<br> часов</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="panel-accompaniment-black">
+        <div class="container">
+          <p>Мы занимается не только созданием сайтов, но также осуществляем дальнейшее сопровождение ресурса и его постоянную техническую поддержку.</p>
+          <p>Услуги по техподдержке сайтов избавят вас от необходимости держать штат специалистов — наполнением, администрированием, дизайном и анализом ресурса, а также другими задачами, необходимыми для его успешной работы, будет заниматься целая команда профессионалов.</p>
+          <div class="accompaniment-description-title">ПО НЕОБХОДИМОСТИ РАБОТЫ ПО ВАШЕМУ САЙТУ<br> ВЫПОЛНЯЕТ ЦЕЛАЯ КОМАНДА:</div>
+          <div class="accompaniment-team-block">
+	        <ul class="list-inline accompaniment-team">
+	          <li><span class="icon-designer">Дизайнер</span></li>
+	          <li><span class="icon-illustrator">Иллюстратор</span></li>
+	          <li><span class="icon-verstalschik">Верстальщик</span></li>
+	          <li><span class="icon-analitik">Аналитик</span></li>
+	          <li><span class="icon-programmer">Программист</span></li>
+	          <li><span class="icon-copywriter">Копирайтер</span></li>
+	          <li><span class="icon-optimizer">Оптимизатор</span></li>
+	        </ul>
+          </div>
+        </div>
+      </div>
+      <div class="services-description accompaniment-description">
+        <div class="container">
+          <div class="col-xs-6 accompaniment-description-column">
+            <div class="h4">Оптимальный пакет</div>
+            <div class="accompaniment-price clearfix">
+	          <div class="accompaniment-price-red">
+	            <div class="accompaniment-price-thousands">9</div>
+	            <div class="accompaniment-price-point">.</div>
+	            <div class="accompaniment-price-red-right">
+	              <div class="accompaniment-price-zeros">000</div>
+	              <div class="accompaniment-currency">руб/мес</div>
+	            </div>
+	          </div>
+            </div>
+            <p class="accompaniment-desc-text">Тариф для компаний, которым важна регулярная работа над обновлением контента сайта, а так же всесторонняя техническая поддержка.</p>
+            <div class="services-link-portfolio"><a href="#" class="red-link">Посмотреть набор услуг</a></div>
+            <div class="services-link-dogovor"><a href="#" class="black-link">Скачать договор</a></div>
+            <a href="#order" class="btn btn-black-red to-order">Заказать</a>
+          </div>
+          <div class="col-xs-6 accompaniment-description-column">
+            <div class="h4">Безлимитный пакет</div>
+            <div class="accompaniment-price clearfix">
+	          <div class="accompaniment-price-red two-digits">
+	            <div class="accompaniment-price-thousands">25</div>
+	            <div class="accompaniment-price-point">.</div>
+	            <div class="accompaniment-price-red-right">
+	              <div class="accompaniment-price-zeros">000</div>
+	              <div class="accompaniment-currency">руб/мес</div>
+	            </div>
+	          </div>
+            </div>
+            <p class="accompaniment-desc-text">Тариф, в первую очередь нацеленный на увеличение продаж с сайта (подходит для интернет-магазинов). Включает в себя комплексное обновление контента и техническую поддержку.</p>
+            <div class="services-link-portfolio"><a href="#" class="red-link">Посмотреть набор услуг</a></div>
+            <div class="services-link-dogovor"><a href="#" class="black-link">Скачать договор</a></div>
+            <a href="#order" class="btn btn-black-red to-order">Заказать</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="services-engineering" id="services-engineering">
+      <div class="h2 bottom-red-line">Проектирование</div>
+      <div class="services-engineering-text">
+        <div class="container">От того, насколько профессионально будет спроектирован сайт компании, зависит успешность и эффективность дальнейшей эксплуатации ресурса и та польза, которую он принесет его владельцу. Поэтому компания Webtrend комплексно подходит к реализации заказов по проектированию сайтов, выделяя для этого все необходимые ресурсы и лучшие возможности своей команды.</div>
+      </div>
+      <div class="services-workflow services-engineering-img">
+        <img src="./images/engineering-algorithm.png" alt=""><!-- 1043px -->
+      </div>
+      <div class="engineering-slider-case">
+	    <img src="./images/slider-case-1.jpg" alt="">
+      </div>
+      <div class="panel-red panel-services-engineering">
+        <div class="container">
+          <div class="engineering-price-block">
+            <span class="engineering-price-from">от</span>
+	        <div class="engineering-price"></div>
+	        <span class="engineering-price-currency">руб.</span>
+          </div>
+          <a href="order-consultation" class="btn btn-white-black order-consultation">Заказать консультацию</a>
+        </div>
+      </div>
+    </div>
+    <div class="services-analitics" id="services-analitics">
+      <div class="h2 bottom-red-line">Аналитика</div>
+      <div class="services-analitics-text">
+        <div class="container">
+          <p>Вы когда-нибудь задумывались о том, что делают посетители на вашем сайте, и почему заказы делает 1% посетителей, а не 5%? 
+Что нужно изменить на сайте, чтобы повысить количество заказов? Вы знаете, что сделать, чтобы реклама приносила больше прибыли?</p>
+          <p>Мы предоставляем услуги в области веб-аналитики, которые дадут ответы на эти вопросы.</p>
+        </div>
+      </div>
+      <div class="analitics-parts">
+	    <div class="container">
+	      <div class="col-xs-12 col-sm-6">
+	        <div class="analitics-one-part">
+		      <div class="analitics-icon icon-audit">
+		        <div class="analitics-icon-border"></div>
+		        Аудит сайта
+		      </div>
+		      <span>Подробный анализ сайта для его дальнейшего продвижения и улучшения для пользователей. Сюда входит не только предварительный анализ сайта для поднятия позиций, но и улучшение юзабилити и контента в целом для пользователей.</span>
+	        </div>
+	        <div class="analitics-one-part">
+		      <div class="analitics-icon icon-counters">
+		        <div class="analitics-icon-border"></div>
+		        Установка и настройка счетчиков
+		      </div>
+		      <span>Счётчик — это специальный скрипт, который передаёт данные о посещаемости вашего сайта в какой-либо сервис статистики</span>
+	        </div>
+	      </div>
+	      <div class="col-xs-12 col-sm-6">
+	        <div class="analitics-one-part">
+		      <div class="analitics-icon icon-testing">
+		        <div class="analitics-icon-border"></div>
+		        Тестирование сайта
+		      </div>
+		      <span>Используют перед запуском большого проекта, чтобы получить гарантии правильной работоспособ- ности, чтобы повысить его эффективность</span>
+	        </div>
+	        <div class="analitics-one-part">
+		      <div class="analitics-icon icon-traffic">
+		        <div class="analitics-icon-border"></div>
+		        Анализ источников трафика
+		      </div>
+		      <span>Для того чтобы понять, насколько эффективен каждый источник трафика, ведущий на сайт, необходимо оценить его показатели</span>
+	        </div>
+	      </div>
+	    </div>
+      </div>
+      <div class="analitics-container container">
+        <div class="analitics-final-text">Стоимость рассчитывается индивидуально исходя из задач</div>
+        <a href="order-consultation" class="btn btn-black-red order-consultation">Заказать консультацию</a>
+      </div>
+    </div>
+  </div>
+</body>  
+  
+  
+  
+  
+  
+  
+<script type="text/javascript">
+  $(window).scroll(function (){
+    if ($(this).scrollTop() > 464){
+      $(".panel-services-menu").fadeIn();
+	} else{
+	  $(".panel-services-menu").fadeOut();
+	}
+	  return false;
+  });
+  
+  $('.panel-services-menu a').on("click", function(){
+    var halo = $(this).attr("name");
+    var scrollanchor = $("#"+ halo).offset().top;
+    var scrollall = scrollanchor - 275;
+    $("body,html").animate({"scrollTop":scrollall}, 'slow');
+  });
+</script>  
+  
+  
+  
+  
+  
+  
+  
+  
+  
